557df530d4eb08fc9b4818cf69b6be59bb40ea05
shivasupraj <shivasupraj.p@gmail.com>
2018-03-20 00:09:17 -0500

    Added the remaining functionality of the user

--- TMS/accounts/models.py
+++ TMS/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save # we will now define signals so our Profile model will be 
+from django.db.models.signals import post_save # we will now define signals so our Profile model will be
 from django.dispatch import receiver           #automatically created/updated when we create/update User instances.
 from django.contrib.auth.models import User
 
@@ -10,16 +10,21 @@ class Profile(models.Model):
     gender = models.CharField(max_length=10)
     job_position = models.CharField(max_length=20)
     salary = models.IntegerField()
-    
+
     def __str__(self):
         return self.emp_name
 
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
     employees = models.ManyToManyField(Profile)
-    
+
     def __str__(self):
         return self.project_name
+
+class supervisor(model.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
 
 class TimeSheet(models.Model):
     user = models.CharField(max_length=100)
@@ -27,6 +32,6 @@ class TimeSheet(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
     project = models.CharField(max_length=100, default='No Project')
-    
+
     def __str__(self):
         return self.user +' '+ self.project +' '+ str(self.date)
--- TMS/accounts/views.py
+++ TMS/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .models import Profile, Project, TimeSheet
 from django.contrib.auth.models import User
-
+import datetime
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
@@ -18,7 +18,7 @@ def login_view(request):
             return render(request, 'accounts/login.html', {'error': 'Username or Password is incorrect'})
     else:
         return render(request, 'accounts/login.html')
-    
+
 def project_view(request):
     if request.user.is_authenticated:
         username = User.objects.get(username=request.user.username)
@@ -32,7 +32,7 @@ def timesheet_view(request):
     if request.user.is_authenticated:
         username = request.user.username
         timesheets = TimeSheet.objects.filter(user=username)
-        print(timesheets[0])
+        print(timesheets)
         return render(request, 'accounts/timesheet.html', {'timesheets':timesheets})
     else:
         return render(request, 'accounts/login.html', {'error': 'Login to access the page'})
@@ -44,28 +44,44 @@ def add_timesheet(request):
             timesheet.user = request.user.username
             timesheet.date = request.POST['date']
             timesheet.start_time = request.POST['from']
-            timesheet.end_time = request.POST['to']             
+            timesheet.end_time = request.POST['to']
             timesheet.project = request.POST.get('project_name')
             timesheet.save()
-            projects = Project.objects.filter(employees__id=request.user.id)            
+            projects = Project.objects.filter(employees__id=request.user.id)
             return render(request, 'accounts/project.html', {'projects':projects})
         else:
             project_name = request.GET.get('project_name')
             return render(request, 'accounts/add_timesheet.html', {'project_name':project_name})
     else:
         return render(request, 'accounts/login.html', {'error': 'Login to access the page'})
-    
+
 def edit_timesheet(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
             timesheet_id = request.POST.get('timesheet_id')
+            timesheet = TimeSheet.objects.filter(id=timesheet_id)[0]
+            timesheet.user = request.user.username
+            timesheet.date = request.POST['date']
+            timesheet.start_time = request.POST['from']
+            timesheet.end_time = request.POST['to']
+            timesheet.project = request.POST.get('project_name')
+            timesheet.save()
+            username = request.user.username
+            timesheets = TimeSheet.objects.filter(user=username)
+            return render(request, 'accounts/timesheet.html', {'timesheets':timesheets})
         else:
             timesheet_id = request.GET.get('timesheet_id')
             timesheet = TimeSheet.objects.filter(id=timesheet_id)[0]
+            grace_days = datetime.datetime.now().date()-timesheet.date
+            # if the requested timesheet's date to be edited is more than 13 days or 2 weeks
+            # then return the timesheets apge to user saying that, he canot edit that timesheet.
+            if grace_days.days > 13:
+                timesheets = TimeSheet.objects.filter(user=request.user.username)
+                return render(request, 'accounts/timesheet.html', {'error':'You can\'t edit your two weeks past timesheet', 'timesheets':timesheets})
             return render(request, 'accounts/edittimesheet.html', {'timesheet':timesheet})
     else:
         return render(request, 'accounts/login.html', {'error': 'Login to access the page'})
-    
+
 def profile_view(request):
     if request.user.is_authenticated:
         username = User.objects.get(username=request.user.username)
@@ -73,9 +89,9 @@ def profile_view(request):
         return render(request, 'accounts/profile.html', {'profile':profile[0]})
     else:
         return render(request, 'accounts/login.html', {'error': 'Login to access the page'})
-    
 
-    
+
+
 def logout_view(request):
     logout(request)
     return render(request, 'accounts/login.html', {'error':'logged out succesfully'})
